e25fd24d74cde3ae238722f6e0e9c8203daa8c05
DorianCalvarin <dorian.calvarin@ynov.com>
2023-01-05 10:08:01 +0100

    app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './astyle.css';
 import Header from './components/Header.js';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <Element>
+      <div className="App">
+        <Header></Header>
+      </div>
+    </Element>
   );
 }
+
+const Element = styled.div`
+  .App {
+    background-color: red;
+  }
+;`
+
 
 export default App;
