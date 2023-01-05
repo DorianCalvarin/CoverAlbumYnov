d6bc7762a33b0e8378ae72eeb09336db5deec989
DorianCalvarin <dorian.calvarin@ynov.com>
2023-01-05 14:32:49 +0100

    flip

--- src/components/Flip.js
+++ src/components/Flip.js
@@ -3,25 +3,35 @@ import '../flip.css';
 
 function Flip() {
   return (
-		<div class="content">
-			<div class="RectangleGauche">
-				<h1>Music is also shared through the eyes </h1>
-				<p>
-					Our website allows artists to create unique and original album covers using 
-					our online generative design tool.
-				</p>
+		<div class="all">
+			<div class="content">
+				<div class="RectangleGauche">
+					<h1>Music is also shared through the eyes </h1>
+					<p>
+						Our website allows artists to create unique and original album covers using 
+						our online generative design tool.
+					</p>
+				</div>
+				<div class="Droit">
+					<div class="Menu">
+						<div class="m1">
+							<p>What is Flip ?</p>
+						</div>
+						<div class="m2">
+							<p>Album cover generator</p>
+						</div>
+						<div class="m3">
+							<p>Generative Design</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="Droit">
-				<div class="Menu">
-					<div class="m1">
+			<div class="content2">
+				<div class="Gauche2">
 
-					</div>
-					<div class="m2">
+				</div>
+				<div class="Droit">
 
-					</div>
-					<div class="m3">
-
-					</div>
 				</div>
 			</div>
 		</div>
