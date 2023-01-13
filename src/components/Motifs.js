7b4df7d62e52dd73b2ef3e6a98cd8edf7d679b2d
DorianCalvarin <dorian.calvarin@ynov.com>
2023-01-13 11:05:42 +0100

    menu motif

--- src/components/Motifs.js
+++ src/components/Motifs.js
@@ -9,16 +9,15 @@ function Motifs({setEtape, Etape}) {
             <>
             <div class="Ariane"> 
                 <h1 class="Couleurfond">Motifs</h1>
-                <div class="5rectangles">
-                    <div class="R1"></div>
+                <div class="rectangles5">
                     <div class="R2"></div>
+                    <div class="R1"></div>
                     <div class="R3"></div>
                     <div class="R4"></div>
                     <div class="R5"></div>
                 </div>
             </div>
             </>
-            
             }
         </div>
 
