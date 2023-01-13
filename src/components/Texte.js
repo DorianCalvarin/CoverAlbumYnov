02dde0f5658f9cbaf4918878aa920dba5d7aa9b2
HugoYnov92 <hugo.labbe@ynov.com>
2023-01-13 10:53:46 +0100

    fnopa

--- src/components/Texte.js
+++ src/components/Texte.js
@@ -9,9 +9,9 @@ function Texte(setEtape, Etape) {
             <div class="Ariane"> 
                 <h1 class="Couleurfond">Texte</h1>
                 <div class="rectangles5">
-                    <div class="R1"></div>
-                    <div class="R2"></div>
                     <div class="R3"></div>
+                    <div class="R2"></div>
+                    <div class="R1"></div>
                     <div class="R4"></div>
                     <div class="R5"></div>
                 </div>
