ddc23bedafa3333ee31874f09a77d0c76d3dd12c
HugoYnov92 <hugo.labbe@ynov.com>
2023-01-13 10:47:40 +0100

    push

--- src/components/Texte.js
+++ src/components/Texte.js
@@ -4,6 +4,19 @@ function Texte(setEtape, Etape) {
     return (
         <div>
             {Etape === 3 && <h1>HELLO</h1>}
+
+            <>
+            <div class="Ariane"> 
+                <h1 class="Couleurfond">Texte</h1>
+                <div class="rectangles5">
+                    <div class="R1"></div>
+                    <div class="R2"></div>
+                    <div class="R3"></div>
+                    <div class="R4"></div>
+                    <div class="R5"></div>
+                </div>
+            </div>
+            </>
         </div>
 
     );
