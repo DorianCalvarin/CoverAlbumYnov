af90cf05a7cf2d7be6ae0d3aad97c4ecfa79a343
DorianCalvarin <dorian.calvarin@ynov.com>
2023-01-16 16:44:35 +0100

    classsname

--- src/components/Filtre.js
+++ src/components/Filtre.js
@@ -4,15 +4,15 @@ function Filtre({setEtape, Etape}) {
     return (
         <div>
             <>
-            <div class="Ariane"> 
-            {Etape === 4 && <h1 class="Couleurfond">Filtre</h1>}
+            <div className='Ariane'> 
+            {Etape === 4 && <h1 className='Couleurfond'>Filtre</h1>}
             {Etape === 4 && 
-                <div class="rectangles5">
-                    <div class="R4"></div>
-                    <div class="R2"></div>
-                    <div class="R3"></div>
-                    <div class="R1"></div>
-                    <div class="R5"></div>
+                <div className='rectangles5'>
+                    <div className='R4'></div>
+                    <div className='R2'></div>
+                    <div className='R3'></div>
+                    <div className='R1'></div>
+                    <div className='R5'></div>
                 </div>
                 }
             </div>
--- src/components/Fond.js
+++ src/components/Fond.js
@@ -7,26 +7,26 @@ function Fond({setEtape, Etape}) {
     return (
         <div>
                 <>
-            <div class="Ariane"> 
-            {Etape === 1 && <h1 class="Couleurfond">Couleur de fond</h1>}
+            <div className='Ariane'> 
+            {Etape === 1 && <h1 className='Couleurfond'>Couleur de fond</h1>}
             {Etape === 1 && 
-                <div class="rectangles5">
-                    <div class="R1"></div>
-                    <div class="R2"></div>
-                    <div class="R3"></div>
-                    <div class="R4"></div>
-                    <div class="R5"></div>
+                <div className='rectangles5'>
+                    <div className='R1'></div>
+                    <div className='R2'></div>
+                    <div className='R3'></div>
+                    <div className='R4'></div>
+                    <div className='R5'></div>
                 </div>
                 }
             </div>
 
             
-            <form class="BlocColorPicker">
+            <form className='BlocColorPicker'>
                 {Etape === 1 &&
-                <p class="TextColorPicker">Choisi la couleur de fond de ta pochette :</p>}
+                <p className='TextColorPicker'>Choisi la couleur de fond de ta pochette :</p>}
                 {Etape === 1 &&
                 <input type="color" id="color-picker" name="color-picker" onChange={(event)=>setCouleur(event.target.value)}/>}
-                <div class="Album" style={{backgroundColor: Couleur}}></div>
+                <div className='Album' style={{backgroundColor: Couleur}}></div>
             </form>
         
             
